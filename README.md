b1888eed6e3fdaac74f3491b4d6267e3b6f9f0ad
lele394 <lele394prod@gmail.com>
2024-12-22 02:21:53 +0100

    submission-ready

---
+++ README.md
@@ -0,0 +1,126 @@
+# OpenMP and OpenACC N-Body simulation with GPU acceleration on GPU
+
+
+![logo](rsc/logo.png)
+
+> The following is a project made for the High Performance Computation course of Master 2 CompuPhys at the Unviersity of Franche-Comté. 
+
+Please report to the following [report](./Léo%20Bechet%20N-Body%20HPC%20report.pdf) for more explanations.
+
+
+## Demo
+
+A python script called `main.py` can be found inside the `program` folder. Executing it will  display a demo menu for the code. Multiple options are available to fine tune your experience.
+
+## Advanced usage
+
+### Loading initial data
+
+Data must be raw dumps, usually from numpy. Below, an example on how to create a sphere 
+
+```python
+import numpy as np
+
+n_stars = 3000
+tallness = 1 #galaxy sim shennanigans
+
+def generate_points_sphere(n_points):
+    points = []
+    while len(points) < n_points:
+        x, y, z = np.random.uniform(-radius, radius, 3)
+        if x**2 + y**2 + z**2 <= radius**2:
+            points.append([x, y, z])
+    return points
+
+def generate_velocity(pos_data, omega_z=1):
+    x, y, z = pos_data[:, 0], pos_data[:, 1], pos_data[:, 2]
+    vx = -omega_z * y
+    vy = omega_z * x
+    vz = np.zeros_like(z)  
+    velocities = np.column_stack((vx, vy, vz))
+    return velocities
+
+pos_data = np.array(generate_points_sphere(n_stars))
+pos_data.T.tofile('positions.dat')
+vel_data = generate_velocity(pos_data)
+vel_data.T.tofile('velocities.dat')
+```
+
+### Using the code
+
+#### OpenMP/CPU compilation
+Compilation is usually done using the following `gfortran -O3 -fopenmp -o out.o sim.f90`. Please feel free to adapt it to your usage.
+
+#### OpenACC/GPU compilation
+Compilation is usually done using the following `pgfortran -O3 -gpu=cc75 -acc -o out.o sim.f90`. Please feel free to adapt it to your usage.
+
+#### Running a simulation
+
+Please make sure the following is properly setup :
+
+Files :
+The following files must be present. You can change their names and path in the source fortran code.
+- `positions.dat` : Initial positions
+- `velocities.dat` : Initial velocities
+
+Settings :
+Please set the corresponding parameters inside the Fortran source code.
+- `n_stars` : The number of stars in the initial files. A wrong value can break the loading of the data, resulting in positions and velocities to be totally wrong.
+- `do_V` : Switch to enable outputting the velocities.
+- `do_E` : Switch to enable outputting the potential and kinetic energies.
+- `save_last_V` : If enabled, the velocities will be dumped at the end of the simulation.
+
+Other settings can also be modified, such as the time step `dt`, the  `number_of_steps`, the `mass`
+
+
+##### Plotting the data
+
+###### Energy
+It is possible to plot the evolution of the energy. Below is a sample script :
+
+```python
+import numpy as np
+import matplotlib.pyplot as plt
+
+data = np.loadtxt('energy.dat')
+
+potential_energy = data[:, 0]
+kinetic_energy = data[:, 1]
+
+total_energy = kinetic_energy + potential_energy
+plt.figure(figsize=(10, 6))
+
+plt.plot(kinetic_energy, label='Kinetic Energy', color='blue')
+plt.plot(potential_energy, label='Potential Energy', color='red')
+plt.plot(total_energy, label='Total Energy', color='green', linestyle='--')
+
+plt.xlabel('Time Step (T)')
+plt.ylabel('Energy')
+plt.title('Kinetic, Potential, and Total Energy vs Time')
+plt.legend()
+
+plt.grid(True)
+plt.show()
+```
+
+##### Animation of the simulation
+
+A sample plotting script is available in the `program` folder, which is a modified (data loading) version of Barnabé DEFORET's code.
+
+## Additional notes
+
+Multiple development versions are available in the `development` folder
+
+
+### Array index switching
+
+We currently have unresolved issues with loading the data when switching the indices of the array. However, the simulation is working as expected. For this reason the available code does not include the array index switch optimization. Benchmarks have been done, and the  
+
+
+
+
+
+
+
+
+
---
+++ program/plot.py
@@ -0,0 +1,165 @@
+#!/usr/bin/env python3
+
+# Author: Barnabé DEFORET
+
+speed = int(2)
+
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation
+
+####################################
+#             Functions            #
+####################################
+
+# def load_file(filename: str, nbline: int = 0, timestep: int = -1, param: list = []):
+#     # This function load the data from the position file. It is designed to read data for a specific time step.
+#     #
+#     # Args:
+#     #     filename (str): path to the file
+#     #     nbline (int): number of particles in a line
+#     #     timestep (int): time step to read
+#     #     param, (list): parameters, i.e number of particles, number of time steps, float precision.
+#     #
+#     # Return:
+#     #     (np.array): Array containing the x, y, z positions of the particles
+
+#     if timestep == -1:
+#         # Read the first 3 integers of the file (number of particles, number of time steps, float precision)
+#         data = np.loadtxt(filename, max_rows=1, dtype=np.int32)
+#     else:
+#         # Read the data for the time step t
+#         data = np.loadtxt(filename, skiprows=1+timestep, max_rows=1, dtype=np.float64)
+#         data = data.reshape(3, nbline)
+#     return data
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def load_file(filename: str, nbline: int = 0, timestep: int = -1, param: list = []):
+    with open(filename, "rb") as file:
+        if timestep == -1:
+            # Read the first 3 integers of the file (metadata)
+            record_size = np.fromfile(file, dtype=np.int32, count=1)  # Skip record size marker
+            metadata = np.fromfile(file, dtype=np.int32, count=3)    # Read metadata
+            record_size = np.fromfile(file, dtype=np.int32, count=1)  # Skip end record marker
+            return metadata
+        else:
+            # Validate 'param'
+            if param is None or len(param) < 3:
+                raise ValueError("Parameter list 'param' must include [num_particles, num_timesteps, float_precision].")
+            
+            num_particles, num_timesteps, float_precision = param
+            dtype = np.float32 if float_precision == 4 else np.float64
+            record_bytes = 4 + (num_particles * 3 * dtype().nbytes) + 4
+            
+            # Skip records before the desired timestep
+            file.seek(4 + (record_bytes * timestep), 0)  # 4 bytes for record size + N records
+            
+            # Read the data for the timestep
+            _ = np.fromfile(file, dtype=np.int32, count=1)  # Skip record size marker
+            data = np.fromfile(file, dtype=dtype, count=num_particles * 3)  # Read positions
+            _ = np.fromfile(file, dtype=np.int32, count=1)  # Skip end record marker
+            
+            # Reshape data into (3, nbline) format (x, y, z)
+            data = data.reshape(3, nbline)
+            return data
+
+
+
+
+
+
+
+
+
+
+
+def anim_realTime(file: str, figs: tuple):
+    # This function create an animation of the particles in real time.
+    #
+    # Args:
+    #     file (str): path to the file
+    #     figs (tuple): size (x, y) of the output GIF (in pixels)
+
+
+    def update_graph(t: int):
+        # Thus function update the position of the particles for time step t
+        #
+        # Args:
+        #     t (int): time step to read
+
+        data = load_file(file, param[0], t, param)
+        graph._offsets3d = (data[0, :], data[1, :], data[2, :])
+
+    # Get parameters
+    param = load_file(file)
+
+    # Load first time step data
+    data = load_file(file, param[0], 0, param)
+
+    # Convert figure size to inches
+    dpi_v = 100
+    sfig = (figs[0]/dpi_v, figs[1]/dpi_v)
+
+    # Create figure
+    fig = plt.figure(figsize = sfig, facecolor = 'k', num = 1)
+    ax = fig.add_subplot(111, projection='3d')
+    ax.set_box_aspect([1, 1, 1])
+    ax.axes.set_xlim3d(left=-3, right=3) 
+    ax.axes.set_ylim3d(bottom=-3, top=3) 
+    ax.axes.set_zlim3d(bottom=-3, top=3)
+    ax.tick_params(axis='x', colors='w', which='both')
+    ax.tick_params(axis='y', colors='w', which='both')
+    ax.tick_params(axis='z', colors='w', which='both')
+    ax.spines['left'].set_color('w')
+    ax.spines['right'].set_color('w')
+    ax.spines['top'].set_color('w')
+    ax.spines['bottom'].set_color('w')
+    ax.xaxis.set_pane_color((1.0, 1.0, 1.0, 0.0))
+    ax.yaxis.set_pane_color((1.0, 1.0, 1.0, 0.0))
+    ax.zaxis.set_pane_color((1.0, 1.0, 1.0, 0.0))
+    ax.xaxis._axinfo["grid"]['color'] =  (0.25,0.25,0.25,1)
+    ax.yaxis._axinfo["grid"]['color'] =  (0.25,0.25,0.25,1)
+    ax.zaxis._axinfo["grid"]['color'] =  (0.25,0.25,0.25,1)
+    ax.set_facecolor("k")
+    plt.tight_layout(pad=1)
+
+    # Plot data
+    graph = ax.scatter(data[0, :], data[1, :], data[2, :], s=2, c='w', linewidths=0)
+
+    # Animate
+    ani = matplotlib.animation.FuncAnimation(fig, update_graph, param[1],
+                            interval=1, blit=False)
+                            
+    plt.show()
+
+if __name__ == '__main__':
+
+    ####################################
+    #            Parameters            #
+    ####################################
+
+    # Path to the file
+    filepath_pos = 'position.dat'
+
+    # Size of the output plot (in pixels)
+    figs = (720, 720)
+
+    ####################################
+    #               Main               #
+    ####################################
+
+    anim_realTime(filepath_pos, figs)
